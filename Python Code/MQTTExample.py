3c932a551c99b08c789ee191d4a314361ce444f4
mcpijnappel <85738767+mcpijnappel@users.noreply.github.com>
2022-01-07 12:05:01 +0100

    Update MQTTExample.py

--- Python Code/MQTTExample.py
+++ Python Code/MQTTExample.py
@@ -1,16 +1,23 @@
 import paho.mqtt.client as mqtt
 
-MQTT_HOST = "192.168.1.101"
-MQTT_USER = "ItechLab"
-MQTT_PASSWORD = "ItechLab"
 
+#credentials used for connecting to the MQTT broker. These have to be changed to your own.
+MQTT_HOST = "192.168.1.101" #ip of the MQTT broker
+MQTT_USER = "ItechLab" #User created for MQTT broker
+MQTT_PASSWORD = "ItechLab" #Password for the created MQTT Broker
+
+
+
+#Things the code has to do when it has connected to the broker
 def on_connect(client, userdata,flags,rc):
     print("Connected with result code "+str(rc))
     client.subscribe("ItechExample/ButtonPressed")
 
+#things the programm does when a message is received on one of the subscribed topics
 def on_message(client, userdata, msg):
     print(msg.topic+" "+str(msg.payload))
 
+#Function that does all the stuff for connecting to the broker    
 def my_connect():
     client = mqtt.Client()
     client.on_connect = on_connect
@@ -19,11 +26,6 @@ def my_connect():
     client.connect(MQTT_HOST, port=1883)
     client.loop_start()
 
-
-#def my_subscribe(client, message_store = None):
-#    client.subscribe("ItechExample/ButtonPressed")
-
 client = my_connect()
-#my_subscribe(client)
 
 print("test")
