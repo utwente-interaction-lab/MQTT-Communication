3d6782aa3a48d4dc4d1615952a934edcac07be15
mcpijnappel <85738767+mcpijnappel@users.noreply.github.com>
2022-01-13 13:04:24 +0100

    Update MQTT_example.ino

--- ESP32 Code/MQTT_example.ino
+++ ESP32 Code/MQTT_example.ino
@@ -6,57 +6,79 @@ You can take the newest version, but when encountering difficulties with the exa
 
 #include "EspMQTTClient.h"
 
+
 long previousMillis = 0;  // variable to store previous running time
-long interval = 1000; // how many milliseconds the function has to wait before running again
-
-int LedPin = 19;
-String LedState = "off";
-int PotPin = 34;
-int PotValue;
+long interval = 5000; // how many milliseconds the function has to wait before running again
 
 
+int LedPin = 19; // pin where the LED is connected to
+String LedState = "off"; // in what state the LED will start without getting a message
+int PotPin = 34; // pin where the Potentiometer value is read (middle pin)
+int PotValue; // variable where the read potentiometer value is read
+
+
+// values for connecting to the internet and mqtt broker/mosquitto
 EspMQTTClient client(
-  "ItechLab",
-  "ItechLab",
-  "192.168.0.101",  // MQTT Broker server ip
-  "ItechLab",   // Can be omitted if not needed
-  "ItechLab",   // Can be omitted if not needed
+  "ItechLab", // WiFi SSID/name
+  "ItechLab", // WiFi password
+  "192.168.0.3",  // MQTT Broker server ip
+  "test",   // MQTT/mosquitto user
+  "test",   // MQTT/mosquitto password
   "ItechExample",     // Client name that uniquely identify your device
   1883              // The MQTT port, default to 1883. this line can be omitted
 );
 
 void setup() {
+  // starts the serial communication with a baudrate of 115200
   Serial.begin(115200);
+  //initializes the LED pin to output
   pinMode(LedPin, OUTPUT);
+  //initializes the Pot pin to input/reading
   pinMode(PotPin, INPUT);
+  //calls the function related to connecting to mqtt and internet
+  ConnectToWiFi();
+
+
 }
 
 
-
+// functions that is called when connection is made
 void onConnectionEstablished()
 {
 
-  long currentMillis = millis();
+  //long currentMillis = millis();
 
-  // Subscribe to "mytopic/test" and display received message to Serial
+  // Subscribe to ItechExample/LedControll", this toggles the led on and of from the example python code
   client.subscribe("ItechExample/LedControll", [](const String & payload) {
-    Serial.println(payload);
-    LedState = payload;
+    Serial.println(payload); // prints the content of a message to the serial monitor
+    LedState = payload; // Sets LedState to the received message
   });
 }
 
 void loop() {
+  //loops the MQTT stuff do not use delay longer than 1ms in the code. since it will mess with this might break stuff
   client.loop();
+
+  // gets the current runtime
   long currentMillis = millis();
+  
+  //if statement that tracks if x amount of seconds have passed since the last time
   if (currentMillis - previousMillis > interval) {
-    // Publish a message to "mytopic/test"
+    
+    // reads the value of the potentiometer
     PotValue = analogRead(PotPin);
+    
+    //prints that value to the serial monitor
     Serial.println(PotValue);
+
+    //sends/publishes the data to the MQTT broker/mosquitto
     client.publish("ItechExample/ButtonPressed", String(PotValue), true); // You can activate the retain flag by setting the third parameter to true
-    //Serial.println("test");
+   ;
+   // resets the timer that tracks how much time has passed
     previousMillis = currentMillis; // reset the variable currentMillis to current run time.
   }
 
+  // toggles the LED based on the received message
   if (LedState == "on") {
     digitalWrite(LedPin, LOW);
     Serial.println("LED turned on");
@@ -68,4 +90,16 @@ void loop() {
     LedState = "changed";
   }
 
+}
+
+// function related to network connections (MQTT/WiFi)
+void ConnectToWiFi()
+{
+
+
+  client.enableDebuggingMessages(); // Enable debugging messages sent to serial output
+  client.enableHTTPWebUpdater(); // Enable the web updater. User and password default to values of MQTTUsername and MQTTPassword. These can be overridded with enableHTTPWebUpdater("user", "password").
+  client.enableOTA(); // Enable OTA (Over The Air) updates. Password defaults to MQTTPassword. Port is the default OTA port. Can be overridden with enableOTA("password", port).
+  client.enableLastWillMessage("TestClient/lastwill", "I am going offline");  // You can activate the retain flag by setting the third parameter to true
+  Serial.println(WiFi.macAddress());
 }
